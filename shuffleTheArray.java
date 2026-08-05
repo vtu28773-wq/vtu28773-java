dd6086676af652fe009a50f0ff9a81b210c83ef5
sanjay <vtu28773@veltech.edu.in>
2026-08-05 10:12:50 +0530

    Add files via upload

---
+++ shuffleTheArray.java
@@ -0,0 +1,12 @@
+//shuffle the array
+class shuffleTheArray {
+   public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2*n];
+        int index = 0;
+        for (int i = 0;i < n;i ++){
+            ans[index++] = nums[i];
+            ans[index++] = nums[i+n];
+        }
+        return ans;
+    }
+}
