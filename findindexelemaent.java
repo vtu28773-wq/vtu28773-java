6b79dd38e3768361983bc30564a6c1c525a57322
sanjay <vtu28773@veltech.edu.in>
2026-08-21 10:31:36 +0530

    Update and rename Task1.java to findindexelemaent.java

--- findindexelemaent.java
+++ findindexelemaent.java
@@ -1,8 +1,8 @@
 
 import java.util.Scanner;
-class Task1{
+class findindexelemaent{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+       
         int arr[] = {10,20,30,40,50};
         System.out.println("enter the index: ");
         int i = sc.nextInt();
