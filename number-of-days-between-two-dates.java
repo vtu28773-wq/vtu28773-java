e76cf6a6ff7dd332726e1a7c0c04b63408bb619b
sanjay <vtu28773@veltech.edu.in>
2026-08-21 09:42:39 +0530

    Add files via upload

---
+++ number-of-days-between-two-dates.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+class number-of-days-between-two-dates {
+public int daysBetweenDates(String date1, String date2) {
+         // parse the input strings into LocalDate objects
+        LocalDate d1 = LocalDate.parse(date1);
+        LocalDate d2 = LocalDate.parse(date2);
+         // calculate the absolute difference in days
+        return (int) Math.abs(ChronoUnit.DAYS.between(d1, d2));
+        
+    }
+};
