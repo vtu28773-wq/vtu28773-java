7324f7873eda14d40762bd4f05e6dbda8525c2ec
sanjay <vtu28773@veltech.edu.in>
2026-09-02 09:16:13 +0530

    Update and rename Task8.java to palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Task8 {
+public class polindrome{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
