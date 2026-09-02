22f782f619cb5304f5bc66a54cb08aa0adbc7e5e
sanjay <vtu28773@veltech.edu.in>
2026-09-02 09:17:17 +0530

    Update and rename Task9.java to sumoflastdigits.java

--- sumoflastdigits.java
+++ sumoflastdigits.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Task9{
+public class sumoflastdigits{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
