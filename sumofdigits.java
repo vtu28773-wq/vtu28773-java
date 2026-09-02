eea2fc8f9ac56bb05b659e6e7da4057241d64a63
sanjay <vtu28773@veltech.edu.in>
2026-09-02 09:14:09 +0530

    Update and rename Task6.java to sumofdigits.java

--- sumofdigits.java
+++ sumofdigits.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task6 {
+public class sumofdigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
