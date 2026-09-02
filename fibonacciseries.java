89eb84b9b5df950535c9697aa0c457b7fe8d4ad5
sanjay <vtu28773@veltech.edu.in>
2026-09-02 09:15:34 +0530

    Update and rename Task7.java to fibonacciseries.java

--- fibonacciseries.java
+++ fibonacciseries.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Task7{
+public class fibonacciseries{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
