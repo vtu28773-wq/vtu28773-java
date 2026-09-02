fd18c5a434e5976fc1eb10594dd6ae020d06558a
sanjay <vtu28773@veltech.edu.in>
2026-09-02 09:12:54 +0530

    Update and rename Task5.java to Arraypairs.java

--- Arraypairs.java
+++ Arraypairs.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Task5{
+public class Arraypairs{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
